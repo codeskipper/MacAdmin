444af7fe58ec69adfd8b72b5e7eb7d6eb25ae1b2
codeskipper <12513894+codeskipper@users.noreply.github.com>
2025-11-09 15:15:39 +0100

    some new scripts

---
+++ scripts/dummy.zsh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+
+# dummy script to prevent uninstall of Workspace ONE macOS Native Apps when they run out of scope
+
+
+echo "dummy uninstall script executed, UNINSTALL has been AVOIDED"
+
+exit 0
+
